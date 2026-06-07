52374358fb53cc0217cbfe433412ef8681851b4c
Ikram <ikrammadyaning@gmail.com>
2026-06-07 13:41:02 +0700

    update

--- src/data/sertificates.js
+++ src/data/sertificates.js
@@ -3,7 +3,7 @@ const experiences = [
     id: 1,
     role: "Belajar dasar web development",
     company: "Eksplor Coding",
-    year: "2025",
+    year: "Februari - 2025",
     description: "learn the basics of web development, which are basic HTML and CSS, the core of which is Flexbox",
     link: "https://drive.google.com/file/d/10I6UUNA5fovJ0Ol-k6C0vAUOOdpyukyX/view"
   },
@@ -11,7 +11,7 @@ const experiences = [
     id: 2,
     role: "Belajar dasar Pemograman",
     company: "Dicoding",
-    year: "2026",
+    year: "May - 2026",
     description: "Building a website using HTML, CSS for landing-page Iphone 14 Pro Max",
     link: "https://drive.google.com/file/d/1tKuCdHUIWiIjRLP9OiCYQIh4gQ_YXdfN/view"
   },
@@ -19,9 +19,23 @@ const experiences = [
     id: 3,
     role: "Belajar dasar Pemograman Javascript",
     company: "Dicoding",
-    year: "2026",
-    description: "Participating in software engineering projects and learning industry best practices.",
-    link: "https://www.sertifikat.com/sertifikat/ponpes-sma-ti-hsi-idn-sukabumi-student-intern-2023-09-30-14-00-00"
+    year: "June - 2026",
+    description: "The final assignment in this class is to write neat JS code and we make calculations using Javascript.",
+    link: "https://drive.google.com/file/d/1Wy3PnA-yaa0y95CoUJ6A3aNEvEY72jAq/view?usp=sharing"
+  },
+
+  {
+    id: 4,
+    role: "Introduction to Financial Literacy",
+    company: "Dicoding",
+    year: "April - 2026",
+    description: "learn how to manage finances for the long term using investment or saving methods",
+    link: "https://drive.google.com/file/d/19YYih2-xQKcILUWXuo8kwK8LqP4xvqbv/view?usp=sharing"
+  },
+  {
+    id: 5,
+    role: "Belajar Membuat Front-End Web untuk Pemula",
+    company: "Dicoding",
   }
 ];
 
